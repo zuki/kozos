69f725f141996bd995a627f0e577a13ea5c6bc1d
Keiji Suzuki <zuki.ebetsu@gmail.com>
2019-03-25 09:52:48 +0900

    edit for list-3.1 in step3

--- bootload/main.c
+++ bootload/main.c
@@ -2,14 +2,17 @@
 #include "serial.h"
 #include "lib.h"
 
+volatile int value = 10;
+
 int main(void)
 {
   serial_init(SERIAL_DEFAULT_DEVICE); /* シリアル・デバイスの初期化 */
 
   puts("Hello world!\n");
 
-  putxval(0x10, 0);   puts("\n");
-  putxval(0xffff, 0); puts("\n");
+  putxval(value, 0);   puts("\n");
+  value = 20;
+  putxval(value, 0); puts("\n");
 
   while(1)                            /* 停止 */
     ;
