2a10b6583ac07d781a01d8535caec0a494b322a7
Keiji Suzuki <zuki.ebetsu@gmail.com>
2019-03-28 18:32:07 +0900

    ファイル名の間違いを修正

--- web/download.sh
+++ web/download.sh
@@ -8,8 +8,12 @@ if [ ! -e $1 ]; then
   mkdir -p $1/os
 fi
 
+kzload_files="elf.c elf.h interrupt.c interrupt.h intr.S lib.c lib.h main.c serial.c serial.h srec.c srec.h startup.s types.h uudecode.c uudecode.h vector.c xmodem.c xmodem.h Makefile ld.scr make.sh"
+
+os_files="command.c configure.h extintr.c extintr.h idle.c interrupt.c interrupt.h kozos.c kozos.h memory.c memory.h lib.c lib.h main.c serial.c serial.h startup.s syscall.c syscall.h thread.c thread.h timer.c timer.h types.h ld.scr make.sh Makefile"
+
 ( cd $1/kzload;
-  for f in elf.c elf.h lib.c lib.h main.c serial.c serial.h srec.c srec.h startup.s xmodem.c xmodem.h ld.scr make.sh; do
+  for f in $kzload_files; do
     wget $2/kzload/$f -O $f.euc
   	nkf -w $f.euc > $f
     rm $f.euc
@@ -17,7 +21,7 @@ fi
 )
 
 ( cd $1/os;
-  for f in lib.c lib.h main.c serial.c serial.h startup.s ld.scr make.sh; do
+  for f in $os_files; do
     wget $2/os/$f -O $f.euc
   	nkf -w $f.euc > $f
     rm $f.euc
