7e3c3108c2170357e127475a8a401e546b7e0bca
Keiji Suzuki <zuki.ebetsu@gmail.com>
2019-04-08 13:54:45 +0900

    add new files name to download.sh

--- web/download.sh
+++ web/download.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # ./download.sh NN http://kozos.jp/kozos/h8/h8_NN
 
@@ -10,8 +10,9 @@ fi
 
 bootload_files="defines.h dram.c dram.h elf.c elf.h interrupt.c interrupt.h intr.S intr.h lib.c lib.h main.c serial.c serial.h srec.c srec.h startup.s vector.c xmodem.c xmodem.h Makefile ld.scr"
 
-os_files="arp.c arp.h clock.c command.c consdrv.c consdrv.h defines.h  ethernet.c ethernet.h icmp.c icmp.h interrupt.c interrupt.h intr.s intr.h ip.c ip.h kozos.c kozos.h ld.scr lib.c lib.h main.c memory.c memory.h netdrv.c netdrv.h rtl8019.c rtl8019.h serial.c serial.h startup.s syscall.c syscall.h timer.c timer.h timerdrv.c timerdrv.h vector.c Makefile"
+os_files="arp.c arp.h clock.c command.c consdrv.c consdrv.h defines.h  ethernet.c ethernet.h httpd.c icmp.c icmp.h interrupt.c interrupt.h intr.s intr.h ip.c ip.h kozos.c kozos.h ld.scr lib.c lib.h main.c memory.c memory.h netdrv.c netdrv.h rtl8019.c rtl8019.h serial.c serial.h startup.s syscall.c syscall.h tcp.c tcp.h timer.c timer.h timerdrv.c timerdrv.h vector.c Makefile"
 
+<< COMMENT
 ( cd $1/bootload;
   for f in $bootload_files; do
     wget $2/bootload/$f -O $f.euc
@@ -19,6 +20,7 @@ os_files="arp.c arp.h clock.c command.c consdrv.c consdrv.h defines.h  ethernet.
     rm $f.euc
   done
 )
+COMMENT
 
 ( cd $1/os;
   for f in $os_files; do
